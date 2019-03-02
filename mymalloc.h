a2bab3de3233c53d1e8e5d9ec1b2417389c3d538
Shivam Patel <46139029+Shivam1615@users.noreply.github.com>
2019-03-02 16:42:54 -0500

    Update mymalloc.h

--- mymalloc.h
+++ mymalloc.h
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#ifndef mymalloc
 
+#define malloc( x ) mymalloc( x, __FILE__, __LINE__ )
+#define free( x ) myfree( x, __FILE__, __LINE__ )
+
+void *mymalloc(size_t);
+
+void myfree(void *);
+
+
+#endif
