8e81c89022de8272ba2d0f43116604be35f55e4f
DhanushSG <48163624+DhanushSG@users.noreply.github.com>
2019-03-02 17:00:42 -0500

    Update mymalloc.h

--- mymalloc.h
+++ mymalloc.h
@@ -4,8 +4,8 @@
 
 #define malloc( x ) mymalloc( x, __FILE__, __LINE__ )
 #define free( x ) myfree( x, __FILE__, __LINE__ )
-
-static char mem[4096];
+#define ArraySize 4096
+static char mem[ArraySize];
 
 void *mymalloc(size_t);
 
