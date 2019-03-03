99bd0066d706e4e9cd40bd73def68ac2fb0fefe4
Shivam Patel <46139029+Shivam1615@users.noreply.github.com>
2019-03-03 15:13:09 -0500

    Update mymalloc.c

--- mymalloc.c
+++ mymalloc.c
@@ -1,9 +1,28 @@
 #include "mymalloc.h"
 
 void* mymalloc(size_t memSize){
-  
-  
+
+
+
 }
+
 void* free(void* memAddr){
-  
+
+
+
+}
+
+void InitilizeFunction(){
+
+int i;
+for(i = 0; i < ArraySize; i++ ){
+
+        mem[i] = '0';
+}
+
+struct metadata start;
+start->free = 0;
+start->datasize = 4096-sizeof(metadata);
+mem[0] = start;
+
 }
