c4a92552a66a69b95cb14c8deabe6b5ead0573a7
Shivam Patel <46139029+Shivam1615@users.noreply.github.com>
2019-03-03 16:46:18 -0500

    Update mymalloc.h

--- mymalloc.h
+++ mymalloc.h
@@ -1,8 +1,7 @@
 #include <stdlib.h>
-#ifndef mymalloc
 
 #define malloc( x ) mymalloc( x, __FILE__, __LINE__ )
-#define free( x ) myfree( x, __FILE__, __LINE__ )
+//#define free( x ) myfree( x, __FILE__, __LINE__ )
 #define ArraySize 4096
 
 int storage = 0;
@@ -17,9 +16,6 @@ static char mem[ArraySize];
 
 void *mymalloc(size_t head, char *variable, size_t line1);
 
-void myfree(void *pointer, char *variable2, size_t line2);
+//void myfree(void *pointer, char *variable2, size_t line2);
 
 void InitilizeFunction();
-
-
-#endif
