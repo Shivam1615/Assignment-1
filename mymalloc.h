719467133c966245f4360273d1b32828c9beb518
Shivam Patel <46139029+Shivam1615@users.noreply.github.com>
2019-03-03 14:59:19 -0500

    Update mymalloc.h

--- mymalloc.h
+++ mymalloc.h
@@ -5,11 +5,19 @@
 #define malloc( x ) mymalloc( x, __FILE__, __LINE__ )
 #define free( x ) myfree( x, __FILE__, __LINE__ )
 #define ArraySize 4096
+
+struct metadata{
+int free;
+int datasize;
+};
+
 static char mem[ArraySize];
 
-void *mymalloc(size_t head);
+void *mymalloc(size_t head, );
 
-void myfree(void *pointer);
+void myfree(void *pointer, );
+
+void InitilizeFunction(){
 
 
-#endif
+}
